e68d4ab433f8e8c08950e18b051fee4796249bc8
drhaynes <drhmbp@gmail.com>
2016-02-25 14:11:47 +0000

    Initial Xcode project creation.

---
+++ SearchTests/SearchTests.swift
@@ -0,0 +1,18 @@
+//
+//  SearchTests.swift
+//  SearchTests
+//
+//  Created by David Haynes on 25/02/2016.
+//  Copyright © 2016 Ordnance Survey. All rights reserved.
+//
+
+import XCTest
+@testable import Search
+
+class SearchTests: XCTestCase {
+
+    func testExample() {
+        XCTAssert(true)
+    }
+    
+}
