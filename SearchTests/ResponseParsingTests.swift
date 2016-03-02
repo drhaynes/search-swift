45842a51c21378ed5e36dc019026ba745f4be5f6
drhaynes <drhmbp@gmail.com>
2016-03-02 18:11:40 +0000

    Add result parsing test

--- SearchTests/ResponseParsingTests.swift
+++ SearchTests/ResponseParsingTests.swift
@@ -25,7 +25,7 @@ class ResponseParsingTests: XCTestCase {
 
     private func validateResponse(response: Response) {
         validateHeader(response.header)
-        // TODO: validateResults(response.results)
+        validateResults(response.results)
     }
 
     private func validateHeader(header: Header) {
@@ -43,7 +43,37 @@ class ResponseParsingTests: XCTestCase {
     }
 
     private func validateResults(results: Results) {
-        fail("Not yet implemented")
+        expect(results.dpa.count).to(equal(1))
+        expect(results.dpa.first).toNot(beNil())
+        let result = results.dpa.first!
+        expect(result.language).to(equal("EN"))
+        expect(result.uprn).to(equal("200010019924"))
+        expect(result.address).to(equal("ORDNANCE SURVEY, 4, ADANAC DRIVE, NURSLING, SOUTHAMPTON, SO16 0AS"))
+        expect(result.organisationName).to(equal("ORDNANCE SURVEY"))
+        expect(result.buildingNumber).to(equal("4"))
+        expect(result.thoroughfareName).to(equal("ADANAC DRIVE"))
+        expect(result.dependentLocality).to(equal("NURSLING"))
+        expect(result.postTown).to(equal("SOUTHAMPTON"))
+        expect(result.postcode).to(equal("SO16 0AS"))
+        expect(result.rpc).to(equal("2"))
+        expect(result.xCoordinate).to(equal(437318))
+        expect(result.yCoordinate).to(equal(115539))
+        expect(result.status).to(equal("APPROVED"))
+        expect(result.logicalStatusCode).to(equal("1"))
+        expect(result.classificationCode).to(equal("CO01GV"))
+        expect(result.classificationCodeDescription).to(equal("Central Government Service"))
+        expect(result.localCustodianCode).to(equal(1760))
+        expect(result.localCustodianCodeDescription).to(equal("TEST VALLEY"))
+        expect(result.postalAddressCode).to(equal("S"))
+        expect(result.postalAddressCodeDescription).to(equal("A single address"))
+        expect(result.blpuStateCode).to(equal("2"))
+        expect(result.blpuStateCodeDescription).to(equal("In use"))
+        expect(result.topographyLayerToid).to(equal("osgb1000002682081995"))
+        expect(result.lastUpdateDate).to(equal("01/09/2010"))
+        expect(result.entryDate).to(equal("01/09/2010"))
+        expect(result.blpuStateDate).to(equal("01/09/2010"))
+        expect(result.match).to(equal(0.6))
+        expect(result.matchDescription).to(equal("NO MATCH"))
     }
 
 }
