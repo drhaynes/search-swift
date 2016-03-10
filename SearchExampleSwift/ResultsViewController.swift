1b83c35b304d0ac50cd38d4f2387baa3c9e6286b
drhaynes <drhmbp@gmail.com>
2016-03-10 12:52:03 +0000

    Remove tableview from storyboard. Display results.

--- SearchExampleSwift/ResultsViewController.swift
+++ SearchExampleSwift/ResultsViewController.swift
@@ -13,6 +13,7 @@ import Fetch
 class ResultsViewController: UITableViewController {
 
     var searchService: SearchService!
+    var results: [SearchResult]!
 
     override func viewDidLoad() {
         let apiKey = NSBundle.mainBundle().URLForResource("APIKEY", withExtension: nil).flatMap { url -> String? in
@@ -24,6 +25,9 @@ class ResultsViewController: UITableViewController {
             } ?? ""
 
         self.searchService = SearchService(apiKey: apiKey)
+        self.results = [SearchResult]()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
 
         super.viewDidLoad()
     }
@@ -35,8 +39,40 @@ class ResultsViewController: UITableViewController {
     internal func search(text: String) {
         if(text.utf16.count > 2) {
             searchService.find(text, completion: { (result) in
-                print("\(result)")
+                switch result {
+                case .Success(let response):
+                    self.results = response.results
+                    self.tableView.reloadData()
+                case .Failure(let error):
+                    print("Search error: \(error)")
+                }
             })
         }
     }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("selected: \(indexPath)")
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let result = results[indexPath.row]
+        cell.textLabel?.text = result.address
+        cell.textLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 12)
+        cell.detailTextLabel?.text = "Coords: \(result.xCoordinate), \(result.yCoordinate)"
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 64.0
+    }
+
 }
