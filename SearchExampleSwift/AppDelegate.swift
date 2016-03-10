b47efe90c5cc76f8d33ad5582afcd476f12aec6a
drhaynes <drhmbp@gmail.com>
2016-03-10 11:57:56 +0000

    Add missing files

---
+++ SearchExampleSwift/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  SearchExampleSwift
+//
+//  Created by David Haynes on 10/03/2016.
+//  Copyright © 2016 Ordnance Survey. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
---
+++ SearchExampleSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchExampleSwift
+//
+//  Created by David Haynes on 10/03/2016.
+//  Copyright © 2016 Ordnance Survey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
